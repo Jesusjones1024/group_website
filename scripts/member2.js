06d82e98f757b27774db99687862a3f51d2bfabd
baconandchips <16443280+baconandchips@users.noreply.github.com>
2020-09-17 13:03:20 -0700

    Commit on current changes

--- scripts/member2.js
+++ scripts/member2.js
@@ -1,12 +1,25 @@
-const educationNav = document.getElementById('showEducation')
-const educationList = document.getElementById('educationList')
+const educationNav = document.getElementById("education");
+const workNav = document.getElementById("work");
 
-educationNav.onclick = function(event){
-    event.preventDefault()
-    
-    workList.style.display = 'none'
-    educationList.style.display = 'block'
-    
-    workNav.classList.remove('about-nav-link-active')
-    this.classList.add('about-nav-link-active')
-  }
+const educationList = document.getElementById("educationList");
+const workList = document.getElementById("workList");
+
+educationNav.onclick = function (event) {
+  event.preventDefault();
+
+  workList.style.display = "none";
+  educationList.style.display = "block";
+
+  workNav.classList.remove("about-nav-link-active");
+  this.classList.add("about-nav-link-active");
+};
+
+workNav.onclick = function (event) {
+  event.preventDefault();
+
+  educationList.style.display = "none";
+  workList.style.display = "block";
+
+  educationNav.classList.remove("about-nav-link-active");
+  this.classList.add("about-nav-link-active");
+};
--- scripts/member4.js
+++ scripts/member4.js
@@ -1,1 +1,43 @@
-// alert("wubalubadubdub")
+const educationBtn = document.getElementById("education-button");
+const workBtn = document.getElementById("work-button");
+const gifBtn = document.getElementById("for-fun-button");
+
+const educationShow = document.getElementById("education-show");
+const workShow = document.getElementById("work-show");
+const gifShow = document.getElementById("for-fun-show");
+
+educationBtn.onclick = function (event) {
+  event.preventDefault();
+
+  educationShow.style.display = "block";
+  workShow.style.display = "none";
+  gifShow.style.display = "none";
+
+  this.classList.add("about-nav-link-active");
+  workBtn.classList.remove("about-nav-link-active");
+  gifBtn.classList.remove("about-nav-link-active");
+};
+
+workBtn.onclick = function (event) {
+  event.preventDefault();
+
+  educationShow.style.display = "none";
+  workShow.style.display = "block";
+  gifShow.style.display = "none";
+
+  educationBtn.classList.remove("about-nav-link-active");
+  this.classList.add("about-nav-link-active");
+  gifBtn.classList.remove("about-nav-link-active");
+};
+
+gifBtn.onclick = function (event) {
+  event.preventDefault();
+
+  educationShow.style.display = "none";
+  workShow.style.display = "none";
+  gifShow.style.display = "block";
+
+  educationBtn.classList.remove("about-nav-link-active");
+  workBtn.classList.remove("about-nav-link-active");
+  this.classList.add("about-nav-link-active");
+};
