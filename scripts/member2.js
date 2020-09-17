53421ac87c528ceada1a8bd29bf4dde398b4582b
JesusJones <rosstodrzak@yahoo.com>
2020-09-17 10:20:57 -0700

    updates

--- scripts/member2.js
+++ scripts/member2.js
@@ -1,8 +1,5 @@
 const educationNav = document.getElementById('showEducation')
-const workNav = document.getElementById('showWork')
-
 const educationList = document.getElementById('educationList')
-const workList = document.getElementById('workList')
 
 educationNav.onclick = function(event){
     event.preventDefault()
@@ -11,16 +8,5 @@ educationNav.onclick = function(event){
     educationList.style.display = 'block'
     
     workNav.classList.remove('about-nav-link-active')
-    this.classList.add('about-nav-link-active')
-  }
-  
-
-workNav.onclick = function(event){
-    event.preventDefault()
-    
-    educationList.style.display = 'none'
-    workList.style.display = 'block'
-    
-    educationNav.classList.remove('about-nav-link-active')
     this.classList.add('about-nav-link-active')
   }
