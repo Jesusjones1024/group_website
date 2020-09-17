6610d3267a10f4586f533c1f0003305a3d56b517
Shixin Qin <imbafrank@126.com>
2020-09-18 01:43:59 +0800

    js updates

--- scripts/member3.js
+++ scripts/member3.js
@@ -1,20 +1,25 @@
 const footballImg = document.getElementById('football-img')
 const footballList = document.getElementById('football-list')
-
+const bgImg = document.getElementById('bg-img')
+const bgList = document.getElementById('bg-list')
 
 footballImg.onclick = function(event){
     event.preventDefault()
-    
     if (footballList.style.display === "none") {
         footballList.style.display = "block";
     } else {
         footballList.style.display = "none";
     }
 
-    // footballList.style.display = 'none'
-    // educationList.style.display = 'block'
-    
-    // workNav.classList.remove('about-nav-link-active')
-    // this.classList.add('about-nav-link-active')
-  }
+}
+
+bgImg.onclick = function(event){
+    event.preventDefault()
+    if (bgList.style.display === "none") {
+        bgList.style.display = "block";
+    } else {
+        bgList.style.display = "none";
+    }
+
+}
   
