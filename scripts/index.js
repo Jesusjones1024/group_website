34da59f3c8b17bdd6c5451685f0a976d6eaa1925
Yue <iamyale1994@gmail.com>
2020-09-17 11:32:07 -0700

    push Yue's page

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,20 @@
 const educationNav = document.getElementById('showEducation')
 const workNav = document.getElementById('showWork')
+const gifNav  = document.getElementById('showGif')
 
 const educationList = document.getElementById('educationList')
 const workList = document.getElementById('workList')
+const gifList = document.getElementById('gifList')
 
 educationNav.onclick = function(event){
     event.preventDefault()
     
     workList.style.display = 'none'
     educationList.style.display = 'block'
+    gifList.style.display = 'none'
     
     workNav.classList.remove('about-nav-link-active')
+    gifNav.classList.remove('about-nav-link-active')
     this.classList.add('about-nav-link-active')
   }
   
@@ -20,7 +24,21 @@ workNav.onclick = function(event){
     
     educationList.style.display = 'none'
     workList.style.display = 'block'
+    gifList.style.display = 'none'
     
+    educationNav.classList.remove('about-nav-link-active')
+    gifNav.classList.remove('about-nav-link-active')
+    this.classList.add('about-nav-link-active')
+  }
+
+gifNav.onclick = function(event){
+    event.preventDefault()
+    
+    workList.style.display = 'none'
+    educationList.style.display = 'none'
+    gifList.style.display = 'block'
+    
+    workNav.classList.remove('about-nav-link-active')
     educationNav.classList.remove('about-nav-link-active')
     this.classList.add('about-nav-link-active')
   }
